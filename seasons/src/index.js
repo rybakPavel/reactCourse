229e6bd9b9c510f4897d7f4ee11cde1b600a6db1
Pavel Rybak <wastelander1988@gmail.com>
2019-02-11 19:56:51 +0300

    tecnical changes
    functional component was replaced by class component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (positionError) => console.log(positionError)
-    )
-    return <div>Hi there!</div>;
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            positionError => console.log(positionError)
+        );
+
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
