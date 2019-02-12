b81c5ccd480b3c47a4e1e8295dd1a372926a8019
Pavel Rybak <wastelander1988@gmail.com>
2019-02-12 18:58:39 +0300

    refactoring and adding
    render and state methods was refactored and added with new content to show latitude and error more effective

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,16 +5,31 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        // this is the only time we do use the direct assignment to 'this.state' method
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we must call setState
+                this.setState({ lat: position.coords.latitude });
+
+                //this.state.lat = position.coords.latitude     is VERY bad example!!!
+            },
+            positionError => {
+                this.setState({ errorMessage: positionError.message });
+            }
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            positionError => console.log(positionError)
+        //we must be trying not to use callbacks and other stuff in render() method
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
         );
-
-        return <div>Latitude: </div>;
     }
 }
 
