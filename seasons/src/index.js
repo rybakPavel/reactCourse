db4534abec9de7d4c6b5b665a69a831e0f776223
Pavel Rybak <wastelander1988@gmail.com>
2019-02-13 20:22:31 +0300

    constructor refactored
    state method was called in by alternative way without using constructor method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        // this is the only time we do use the direct assignment to 'this.state' method
-        this.state = { lat: null, errorMessage: '' };      
-    }
+    state = { lat: null, errorMessage: '' };      
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
